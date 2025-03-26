b82e61ef9372902c31f7367c2114e3b36cd16bbb
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-26 14:25:14 +0100

    [UPD] no style input and button added. rewritten script to adapt

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
 // creation of variables
-let distance;
-let age;
-let price = 0.21;
+const distance = document.getElementById("distance").value;
+const age = document.getElementById("age").value;
+const button = document.getElementById("button");
+const price = 0.21;
 
-while (isNaN(distance) || distance < 0) {
-    distance = prompt("Enter the distance in km");
-}
-while (isNaN(age) || age < 0) {
-    age = prompt("Enter your age");
-}
-console.log("Distance: " + distance + " km");
-console.log("Age: " + age + " years");
+button.addEventListener("click", function() {
+    const total = (distance * price).toFixed(2);
+    let finalPrice = 0;
 
-// calculation of the price
-let totalPrice = distance * price;
+    if (age < 18) {
+        finalPrice = total * 0.8;
+    } else if (age > 65) {
+        finalPrice = total * 0.6;
+    } else {
+        finalPrice = total;
+    }
 
-// calculation of the discount
-if (age < 18) {
-    totalPrice = totalPrice * 0.8;
-} else if (age > 65) {
-    totalPrice = totalPrice * 0.6;
-}
-
-// output of the price
-console.log("The price of the ticket is: " + totalPrice.toFixed(2) + "€");
+    console.log(finalPrice);
+});
